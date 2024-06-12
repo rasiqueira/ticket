5fe71e1a159a37559ecf4bf9c6c6879ed2c8396b
Rodrigo Siqueira <34777347+rasiqueira@users.noreply.github.com>
2024-06-12 17:57:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,13 @@ if 'name' not in st.session_state:
     st.session_state['name'] = ''
 if 'username' not in st.session_state:
     st.session_state['username'] = ''
+if 'logout' not in st.session_state:
+    st.session_state['logout'] = False
 
 # Função para carregar e salvar tickets
 def load_tickets():
     if not os.path.exists('data/tickets.csv'):
-        tickets = pd.DataFrame(columns=['user', 'title', 'description', 'status', 'respondent',  'response'])
+        tickets = pd.DataFrame(columns=['user', 'title', 'description', 'status', 'respondent', 'response'])
         tickets.to_csv('data/tickets.csv', index=False)
     return pd.read_csv('data/tickets.csv')
 
@@ -90,6 +92,10 @@ if authentication_status:
 
 elif authentication_status == False:
     st.error('Nome de usuário ou senha incorretos')
+
+elif authentication_status == None:
+    st.warning('Por favor, insira seu nome de usuário e senha')
+
 
 elif authentication_status == None:
     st.warning('Por favor, insira seu nome de usuário e senha')
