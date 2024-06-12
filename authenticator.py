818190d3a156323b94146fb27787f684686a0ad4
Rodrigo Siqueira <34777347+rasiqueira@users.noreply.github.com>
2024-06-12 16:52:37 -0300

    Update authenticator.py

--- authenticator.py
+++ authenticator.py
@@ -1,34 +1,31 @@
-import streamlit_authenticator as stauth
 import streamlit as st
+import streamlit_authenticator as stauth
 
 # Acessar os segredos
 credentials = st.secrets["credentials"]
 
-users = [
-    {"name": credentials["user1_name"], "username": credentials["user1_username"], "password": credentials["user1_password"], "role": credentials["user1_role"]},
-    {"name": credentials["user2_name"], "username": credentials["user2_username"], "password": credentials["user2_password"], "role": credentials["user2_role"]}
-]
-
-names = [user["name"] for user in users]
-usernames = [user["username"] for user in users]
-passwords = [user["password"] for user in users]
-roles = {user["username"]: user["role"] for user in users}
-
-# Gerar senhas hash
-hashed_passwords = stauth.Hasher(passwords).generate()
+names = [credentials["user1_name"], credentials["user2_name"]]
+usernames = [credentials["user1_username"], credentials["user2_username"]]
+passwords = [credentials["user1_password"], credentials["user2_password"]]
+roles = {
+    credentials["user1_username"]: credentials["user1_role"],
+    credentials["user2_username"]: credentials["user2_role"]
+}
 
 # Estrutura de credenciais esperada
+hashed_passwords = stauth.Hasher(passwords).generate()
 credentials_dict = {
     "usernames": {
-        user["username"]: {
-            "name": user["name"],
-            "password": hashed_password
-        } for user, hashed_password in zip(users, hashed_passwords)
+        usernames[i]: {
+            "name": names[i],
+            "password": hashed_passwords[i]
+        } for i in range(len(usernames))
     }
 }
 
 authenticator = stauth.Authenticate(
     credentials_dict,
     'ticket_system',  # Este é o nome da aplicação
-    'abcdef'
+    'abcdef',  # Um segredo utilizado na geração dos cookies
+    cookie_expiry_days=1
 )
